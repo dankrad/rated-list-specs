fa65e032750e8829c58269c6828044611914ae07
chirag-parmar <chiragparmar12209@gmail.com>
2024-08-29 15:38:39 +0530

    fixes

--- simulator/__main__.py
+++ simulator/__main__.py
@@ -1,9 +1,21 @@
 from simulator import Simulator
+from node import Node
+import secrets
+from eth2spec.utils.ssz.ssz_typing import Bytes32
+
+
+def generate_node_ids(count):
+    ids = []
+    for i in range(count):
+        ids.append(secrets.token_bytes(32))
+
+    return ids
 
 
 def main():
-    sim = Simulator()
+    print(generate_node_ids(32))
+    node = Node(secrets.token_bytes(32), generate_node_ids(50))
 
-if __name__=='__main__':
-   main()
-    
+
+if __name__ == "__main__":
+    main()
--- simulator/node.py
+++ simulator/node.py
@@ -1,99 +1,77 @@
-from typing import * 
+from typing import List, Dict, Tuple, Set
+from eth2spec.utils.ssz.ssz_typing import Bytes32, uint64
+from dataclasses import dataclass
 
-NodeId = int
-SampleId = str
-Root = str
+type NodeId = Bytes32
+type SampleId = uint64
+type Root = Bytes32
 
-MAX_CHILDREN = 100
-MAX_PARENT = 100
 
 class NodeRecord:
-    def __init__(self, nodeId):
-        self.nodeId = NodeId
-        self.children : List[NodeRecord] = {}
-        self.parents : List[NodeRecord] = {}
-
-    def add_parent(self, nodeRecord):
-        if self.parents.count >= MAX_PARENT:
-            print("the max parent limit reached for the node")
-            return 
-        self.parents.append(nodeRecord)
-
-    def add_children(self, nodeRecord):
-        if self.children.count >= MAX_CHILDREN:
-            print("the max children limit reached for the node")
-            return 
-        self.children.append(nodeRecord)
-
-class RatedListDHT:
-    """ This class implements the rated list data structure """
-    
-    def __init__(self):
-        self.nodes : Dict[NodeId, NodeRecord] = {}
-        self.scores : Dict[str, ScoreKeeper] = {}
+    def __init__(self, node_id, children, parents):
+        self.node_id: NodeId = node_id
+        self.children: List[NodeRecord] = children
+        self.parents: List[NodeRecord] = parents
 
 
+@dataclass
 class ScoreKeeper:
-    """ This class implements the score keeper data structure"""
-    def __init__(self):
-        self.descendants_contacted: Dict[NodeId,Set[Tuple[NodeId, SampleId]]] = {}
-        self.descendants_replied: Dict[NodeId,Set[Tuple[NodeId, SampleId]]] = {} 
+    """This class implements the score keeper data structure"""
 
+    descendants_contacted: Dict[NodeId, Set[Tuple[NodeId, SampleId]]]
+    descendants_replied: Dict[NodeId, Set[Tuple[NodeId, SampleId]]]
+
+
+@dataclass
+class RatedListDHT:
+    """This class implements the rated list data structure"""
+
+    nodes: Dict[NodeId, NodeRecord]
+    scores: Dict[Root, ScoreKeeper]
 
 
 class Node:
-    """ This class implements a node in the network"""
-    def __init__(self, ID):
-        print(" starting a new node in the network")
-        self.ID = ID
-        self.dht = RatedListDHT()
-        print(" started a node in the node with nodeId - %s",ID)
+    """This class implements a node in the network"""
 
-    def create_empty_node_record(self, id: NodeId) -> NodeRecord:
-        node_record = NodeRecord(
-            nodeId=id
+    def __init__(self, id: NodeId, peers: List[NodeId]):
+        print(" starting a new node in the network")
+
+        self.own_id = id
+        self.dht = RatedListDHT({}, {})
+
+        self.dht.nodes[id] = NodeRecord(id, [], [])
+        self.on_get_peers_response(id, peers)
+
+        print(" started a node in the node with nodeId - %s", id)
+
+    def compute_descendant_score(self, block_root: Root, node_id: NodeId) -> float:
+        score_keeper = self.dht.scores[block_root]
+        return len(score_keeper.descendants_contacted[node_id]) / len(
+            score_keeper.descendants_replied[node_id]
         )
 
-        return node_record
-    
-    """ Here the Root is a bytes32 string"""
-    def compute_descendant_score(self,
-                             block_root: Root,
-                             node_id: NodeId) -> float:
-        score_keeper = self.dht .scores[block_root]
-        return score_keeper.descendants_contacted[node_id] / score_keeper.descendants_replied[node_id]
-    
-    def on_get_peers_response(self, node: NodeId, children: Sequence[NodeId]):
-    
-        for child_id in children:
+    def on_get_peers_response(self, node_id: NodeId, peers: List[NodeId]):
+        for peer_id in peers:
             child_node: NodeRecord = None
 
-            if child_id not in self.dht.nodes: 
-                child_node = self.create_empty_node_record(child_id)
-                self.dht.nodes[child_id] = child_node
+            if peer_id not in self.dht.nodes:
+                child_node = NodeRecord(peer_id, [], [])
+                self.dht.nodes[peer_id] = child_node
 
-            child_node.parents.append(node)
-            
-            if child_id not in self.dht.nodes:
-                self.dht.nodes[child_id] = child_node
+            self.dht.nodes[peer_id].parents.append(node_id)
+            self.dht.nodes[node_id].children.append(child_node)
 
-            self.dht.nodes[node].children.append(child_node)
+        for child in self.dht.nodes[node_id].children:
+            if child.node_id not in peers:
+                # Node no longer has child peer, remove link
+                self.dht.nodes[node_id].children.remove(child)
+                self.dht.nodes[child.node_id].parents.remove(self.dht.nodes[node_id])
 
-        for child_id in self.dht.nodes[node].children:
-            if child_id not in children:
-                self.dht.nodes[node].children.remove(child_id)
-                self.dht.nodes[child_id].parents.remove(node)
-                if len(self.dht.nodes[child_id].parents) == 0:
-                    self.dht.nodes.pop(child_id)
+                if len(child.parents) == 0:
+                    del self.dht.nodes[child.node_id]
 
-
-
-    def compute_node_score(self,
-                       block_root: Root,
-                       node_id: NodeId) -> float:
-        
-
-        score = self.compute_descendant_score(self.dht, block_root, node_id)
+    def compute_node_score(self, block_root: Root, node_id: NodeId) -> float:
+        score = self.compute_descendant_score(block_root, node_id)
 
         cur_path_scores: Dict[NodeId, float] = {
             parent: score for parent in self.dht.nodes[node_id].parents
@@ -101,49 +79,22 @@ class Node:
 
         best_score = 0.0
 
+        # traverse all paths of node_id by iterating through its parents and
+        # grand parents. Note the best score when the iteration reaches root
         while cur_path_scores:
             new_path_scores: Dict[NodeId, float] = {}
             for node, score in cur_path_scores.items():
                 for parent in self.dht.nodes[node].parents:
-                    if parent == self.dht.own_id:
+                    if parent == self.own_id:
                         best_score = max(best_score, score)
                     else:
-                        par_score = self.compute_descendant_score(self.dht, block_root, parent)
-                        if parent not in new_path_scores or new_path_scores[parent] < par_score:
+                        par_score = self.compute_descendant_score(block_root, parent)
+                        if (
+                            parent not in new_path_scores
+                            or new_path_scores[parent] < par_score
+                        ):
                             new_path_scores[parent] = par_score
 
             cur_path_scores = new_path_scores
 
         return best_score
-    
-    def on_get_peers_response(self, node: NodeId, children: Sequence[NodeId]):
-    
-        for child_id in children:
-            child_node: NodeRecord = None
-
-            if child_id not in self.dht.nodes: 
-                child_node = self.create_empty_node_record(child_id)
-                self.dht.nodes[child_id] = child_node
-
-            child_node.parents.append(node)
-            
-            if child_id not in self.dht.nodes:
-                self.dht.nodes[child_id] = child_node
-
-            self.dht.nodes[node].children.append(child_node)
-
-        for child_id in self.dht.nodes[node].children:
-            if child_id not in children:
-                self.dht.nodes[node].children.remove(child_id)
-                self.dht.nodes[child_id].parents.remove(node)
-                if len(self.dht.nodes[child_id].parents) == 0:
-                    self.dht.nodes.pop(child_id)
- 
-
-    
-
-
-
-
-    
-        
